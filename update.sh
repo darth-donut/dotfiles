43a4641b7fa30ab2b3fe929bf67d5f888483ea78
jfong <jfong361@gmail.com>
2017-12-17 03:55:18 +1100

    add auto update script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ~/.bash_aliases .
+echo "Updated .bash_aliases"
+cp ~/.bashrc .
+echo "Updated .bashrc"
+cp ~/config .
+echo "Updated config"
+cp ~/i3blocks.conf .
+echo "Updated i3blocks.conf"
+cp ~/.tmux.conf .
+echo "Updated .tmux.conf"
+cp ~/.vimrc .
+echo "Updated .vimrc"
